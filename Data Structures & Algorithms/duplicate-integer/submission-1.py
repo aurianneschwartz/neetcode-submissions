68866ff55db40c0f64d6a5020e755f7c1ba25aac
Aurianne Schwartz <aurianne.schw@gmail.com>
2026-09-04 18:31:32 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashmap = set()
+        for i in range(len(nums)):
+            if nums[i] in hashmap:
+                return True
+            hashmap.add(nums[i])
+        return False
