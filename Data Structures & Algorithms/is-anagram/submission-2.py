3c8d0b45ddaa9202f4e2dd07b08bf1873782da3d
Aurianne Schwartz <aurianne.schw@gmail.com>
2026-09-05 10:26:12 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hm = defaultdict(int)
+        for char in s:
+            if char not in hm:
+                hm[char] += 1
+            else:
+                hm[char] += 1
+        
+        for char in t:
+            if char not in hm:
+                return False
+            else:
+                hm[char] -= 1
+                if hm[char] < 0:
+                    return False
+        
+        if sum(hm.values()) == 0:
+            return True
+
+        return False
